07fec1120599ffaa3418a24673eedc972ccd57c4
Victor Bäckman <victorbackman2@gmail.com>
2020-04-04 12:00:57 +0200

    WIP with front-end

--- src/webapp/dash_app.py
+++ src/webapp/dash_app.py
@@ -8,9 +8,18 @@ import flask
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__, server=server, url_base_pathname='/optimization/', external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__, server=server, url_base_pathname='/optimization/', assets_folder='assets2')
+app = dash.Dash(__name__, server=server, assets_folder='assets2')
+
+# app.config.suppress_callback_exceptions = True
+# app.layout = html.Div([
+#     dcc.Location(id='url', refresh=False),
+#     html.Div(id='page-content')
+# ])
 
 app.layout = html.Div(children=[
+    dcc.Location(id='url', refresh=False),
     html.H1(children='AI-Mackapären 5000'),
     html.Br(),
     html.H3(children='''
@@ -47,7 +56,25 @@ app.layout = html.Div(children=[
         children=html.Div(id="slider_1_output"),     # change place of this too visualize loading
         type="circle",
     ),
+
+    html.Img(src=app.get_asset_url('banner.jpg')),
+    print(app.get_asset_url('banner.jpg'))
 ])
+
+
+@server.route('/')
+def render_index_page():
+    return flask.send_file('index.html')
+
+
+@server.route('/forecast')
+def render_forecast_page():
+    return flask.send_file('forecast.html')
+
+
+@server.route('/about')
+def render_about_page():
+    return flask.send_file('about.html')
 
 
 @app.callback(
@@ -79,3 +106,20 @@ def update_map(value):
     html_string = m.get_root().render()
 
     return html.Iframe(srcDoc=html_string, width='100%', height='500px')
+
+
+# Update the index
+# @app.callback(dash.dependencies.Output('page-content', 'children'),
+#               [dash.dependencies.Input('url', 'pathname')])
+# def display_page(pathname):
+#     if pathname == '/forecasting':
+#         return 'Hej'
+#     elif pathname == '/optimization':
+#         return layout_optimization
+#     else:
+#         # layout_index = html.Iframe(id='index', srcDoc=open('index.html', 'r').read(), width='100%', height='100%'),
+#         layout_index = html.Div(open('index.html', 'r').read())
+#         # return flask.send_file('index.html')
+#         return layout_index
+#
+#         # You could also return a 404 "URL not found" page here
--- src/webapp/wsgi.py
+++ src/webapp/wsgi.py
@@ -3,7 +3,6 @@ from src.webapp.dash_app import app
 PORT = 5000
 HOST = '0.0.0.0'
 app.secret_key = "SuPerUltrAsaFeKey3000"     # should be set in env, but YOLO
-
 if __name__ == '__main__':
     app.run_server(host=HOST, port=PORT, debug=True)    # if run manually, start the app
 else:
