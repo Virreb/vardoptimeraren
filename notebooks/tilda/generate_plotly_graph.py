6e31e76299e520ad0250579d0e3935118515b1e6
tildalundgren <tilda.lundgren@advectas.se>
2020-04-04 15:26:26 +0200

    Experimenting with plotly graph

--- notebooks/tilda/generate_plotly_graph.py
+++ notebooks/tilda/generate_plotly_graph.py
@@ -19,21 +19,31 @@ def get_data():
 
 
 def plot(results):
-	regioner = list(results['Region'].unique())
-
-	line = go.Scatter(
+    regioner = list(results['Region'].unique())
+    
+    df = pd.read_csv("../../data/iva_kumulativ.csv",sep=";")
+    df_T = df.T
+    df_T.columns = df_T.iloc[0]
+    df_T = df_T.drop(df_T.index[0])
+    df_T = df_T.drop(columns=["Hela riket"])
+    df_T = df_T.reset_index()
+    df_T = df_T.rename(columns={"index": "date"})
+    df_T = pd.melt(df_T, id_vars=["date"],var_name="Region",value_name="iva")
+    temp = df_T.merge(results,on=["date","Region"],how="left")
+    
+    line = go.Scatter(
 	    x=results[results['Region'] == regioner[0]]['date'],
 	    y=results[results['Region'] == regioner[0]]['predicted'],
 	    name='Predicted IVA'
 	)
-
-	line2 = go.Scatter(
+    
+    line2 = go.Scatter(
 	    x=results[results['Region'] == regioner[0]]['date'],
 	    y=results[results['Region'] == regioner[0]]['iva'],
 	    name='Actual IVA'
 	)
-
-	updatemenus = [
+    
+    updatemenus = [
 	    {
 	        'buttons': [
 	            {
@@ -54,17 +64,16 @@ def plot(results):
 	        'showactive': True,
 	    }
 	]
-
-	layout = go.Layout(
+    
+    layout = go.Layout(
 	    updatemenus=updatemenus,
 	)
-
-	figure = go.Figure(data=[line, line2],  layout=layout)
-	figure.update_layout(
+    
+    figure = go.Figure(data=[line, line2],  layout=layout)
+    figure.update_layout(
 	    xaxis_tickformat='%d %B',
 	)
-	
-	figure.write_html('../../plotly_graph.html')
+    figure.write_html('./plotly_graph.html')
 
 
 if __name__=='__main__':
