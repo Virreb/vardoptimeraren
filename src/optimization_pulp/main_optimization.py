9f86b326ac32d6ef9513310c17180f88a87bf8f6
tildalundgren <tilda.lundgren@advectas.se>
2020-04-04 08:05:46 +0200

    Experimenting with PuLP

---
+++ src/optimization_pulp/main_optimization.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 08:59:33 2020
+@author: tilda.lundgren
+"""
+
+def run_optimization(start_day="2020-3-28", 
+                     time_horizon=3, 
+                     path_to_trend_data = "../../data/iva_kumulativ.csv",
+                     path_to_static_region_data = "../../data/regions.csv",
+                     path_to_static_geojson = "../../data/geocounties.geojson",
+                     solve_time_limit = 60, #seconds
+                     w_total_undercapacity = 100,
+                     w_max_under = 100,
+                     w_max_over = 1,
+                     w_nb_patient_transfers = 1,
+                     w_km_patient_transfers = 0.01,
+                     w_nb_long_transfers = 0.01
+                     ):
+    
+    # --------------------------------------------------------------------------- #
+    # Set-up
+    # --------------------------------------------------------------------------- #  
+    import warnings
+    warnings.filterwarnings("ignore")
+    
+    from datetime import timedelta
+    from datetime import datetime
+    
+    today = datetime.strptime(start_day, '%Y-%m-%d')
+    target_day = today + timedelta(days=time_horizon)
+    
+    # --------------------------------------------------------------------------- #
+    # Read data
+    # --------------------------------------------------------------------------- #  
+    import read_data_for_optimization 
+    current_df, trend_dict = read_data_for_optimization.read_and_process_data(trend_data = path_to_trend_data,
+                                                                              region_data = path_to_static_region_data,
+                                                                              today = today)
+    initial_map = read_data_for_optimization.plot_initial_state(current_df,geojson=path_to_static_geojson)    
+    
+    # --------------------------------------------------------------------------- #
+    # Build and prepare optimization model
+    # --------------------------------------------------------------------------- # 
+    import build_optimization_model
+    build_optimization_model.check_environment()
+    mdl = build_optimization_model.build_model()
+    mdl = build_optimization_model.define_model_parameters_and_sets(mdl, current_df)
+    mdl = build_optimization_model.define_model_variables(mdl)
+    mdl = build_optimization_model.calculate_distances(mdl, current_df)
+    
+    import define_model_constraints
+    mdl = define_model_constraints.exempt_departments(mdl)
+    mdl = define_model_constraints.set_initial_state(mdl,current_df)
+    mdl = define_model_constraints.link_x_and_y_vars(mdl)
+    mdl = define_model_constraints.restrict_transfers(mdl)
+    mdl = define_model_constraints.transfer_bounds(mdl)
+    mdl = define_model_constraints.maximum_long_transfers(mdl)
+    mdl = define_model_constraints.short_transfers_per_dep(mdl)
+    mdl = define_model_constraints.update_for_next_period(mdl,trend_dict,today,target_day)
+    
+    import define_model_objective
+    mdl = define_model_objective.define_total_undercapacity(mdl,current_df)
+    mdl = define_model_objective.undercapacity_distribution(mdl,current_df)
+    mdl = define_model_objective.overcapacity_distribution(mdl,current_df)
+    mdl = define_model_objective.define_distance_measures(mdl)
+    mdl = define_model_objective.summarize_objectives(mdl,
+                                                      w_total_undercapacity = w_total_undercapacity,
+                                                      w_max_under = w_max_under,
+                                                      w_max_over = w_max_over,
+                                                      w_nb_patient_transfers = w_nb_patient_transfers,
+                                                      w_km_patient_transfers = w_km_patient_transfers,
+                                                      w_nb_long_transfers = w_nb_long_transfers)
+    
+    # --------------------------------------------------------------------------- #
+    # Solve model
+    # --------------------------------------------------------------------------- # 
+    mdl.set_time_limit(solve_time_limit); #Seconds
+    mdl.parameters.mip.strategy.probe.set(0);
+    mdl.parameters.parallel.set(-1); #  opportunistic parallel search mode
+    mdl.parameters.threads.set(4);
+    mdl.solve(log_output=False,lex_mipgaps = [0.001])    
+    
+    # --------------------------------------------------------------------------- #
+    # Process solution
+    # --------------------------------------------------------------------------- # 
+    import process_solution
+    mdl = process_solution.process_allocations(mdl)
+    mdl,current_df = process_solution.process_final_data(mdl,current_df,trend_dict,today,target_day)
+    final_map = process_solution.plot_final_state(mdl,current_df,geojson=path_to_static_geojson)
+    
+    return initial_map, final_map, mdl.allocation_plan
---
+++ src/optimization_pulp/read_data_for_optimization.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Read data for optimization
+"""
+import pandas as pd
+from datetime import date
+import folium
+ 
+def read_and_process_data(trend_data, region_data, today = date(2020,3,29)):
+    
+    # Read historical Covid-19-data
+    iva_df = pd.read_csv(trend_data,sep=";")
+    iva_df.Region = iva_df.Region.apply(lambda x: x.replace("Region ", ""))
+    iva_df = iva_df.set_index(iva_df.Region)
+    iva_df = iva_df.drop(["Region"],axis=1)
+    iva_df = iva_df.drop('Hela riket')
+    
+    # Process historical data
+    data_df = pd.DataFrame(columns=["Date","Region","IVA"])
+    for ind, row in iva_df.iterrows(): 
+        temp = pd.DataFrame(data = row,index= row.index)
+        temp.columns = ["IVA"]
+        temp["Region"] = ind
+        temp["Date"] = temp.index
+        temp = temp.reset_index(drop=True)
+        data_df = data_df.append(temp)
+    data_df = data_df.reset_index(drop=True)
+    
+    # Read region data
+    region_df = pd.read_csv(region_data,sep=";")
+    region_df.Region = region_df.Region.apply(lambda x: x.replace("Region ", ""))
+        
+    # Extract current still image
+    current_df = data_df[data_df["Date"] == today.strftime("%Y-%m-%d")]
+    
+    # Merge current_df with region data
+    current_df = current_df.merge(region_df, on=['Region'], how='left')
+    
+    # Process current_df
+    current_df["UnderCapacity"] = current_df["IVA"] - current_df["Capacity"]
+    current_df["UnderCapacity"] = current_df["UnderCapacity"].apply(lambda x: max(0,x))
+    current_df["SurplusCapacity"] = current_df["Capacity"] - current_df["IVA"]
+    current_df["SurplusCapacity"] = current_df["SurplusCapacity"].apply(lambda x: max(0,x))
+    current_df["Rate"] = current_df["IVA"] / current_df["Capacity"]
+    current_df = current_df.set_index(current_df.Region)
+    
+    # Region data
+    trend_dict = {}
+    for d in list(region_df.Region):
+        data = data_df[data_df.Region==d]
+        data = data.groupby('Date').sum().reset_index()
+        data['Date']=pd.to_datetime(data['Date'])
+        data = data.sort_values(by=['Date'], ascending=False)
+        trend_dict[d] = data  
+        
+    return current_df, trend_dict
+
+def plot_initial_state(current_df,geojson):
+    
+    # Initiate map
+    m = folium.Map(location=[62, 20], zoom_start=5)
+    
+    # Define styling rules for counties
+    def style_function(feature):
+        d = feature['properties']['name']   
+        
+        if current_df.at[d,"Rate"] < 0.5:
+            if current_df.at[d,"SurplusCapacity"] > 3:
+                color = 'grey'
+            else: 
+                color = 'green'
+        elif 0.5 <= current_df.at[d,"Rate"] < 0.9:
+            color='green'
+        elif 0.9 <= current_df.at[d,"Rate"] <= 1:
+            color='orange'   
+        elif 1 < current_df.at[d,"Rate"]:
+            color='red'  
+            
+        return {'fillOpacity': 0.3,'weight': 0.5,
+                'color': 'black','fillColor': color}
+    
+    # Import geojson data and apply styling rule
+    folium.GeoJson(
+        geojson,
+        name='geojson',
+        style_function=style_function
+    ).add_to(m)
+    
+    # Add a clickable circle to each county
+    for idx, row in current_df.iterrows():           
+        
+        # Define styling rules
+        if row.Rate < 0.5:
+            if row.SurplusCapacity > 3:
+                color = 'grey'
+            else: 
+                color = 'green'
+        elif 0.5 <= row.Rate < 0.9:
+            color='green'
+        elif 0.9 <= row.Rate <= 1:
+            color='orange'   
+        elif 1 < row.Rate:
+            color='red'  
+        
+        # Draw circle
+        folium.Circle(
+            radius= 7000 + row.IVA*200,
+            location=[row.Lat, row.Long],
+            popup=folium.Popup('<b>'+row.Region+'</b><br>'+\
+                               '<br>Patienter: '+str(row.IVA)+\
+                               '<br>Kapacitet: '+str(row.Capacity),
+                               max_width=450,min_width=150),
+            color=color,
+            fill=True,
+            fill_color=color,
+            tooltip="Klicka här!",
+        ).add_to(m)
+    
+    return m
