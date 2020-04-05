7b287c3ecf14805a9119a7a756fda738e5e2f6c3
tildalundgren <tilda.lundgren@advectas.se>
2020-04-05 09:36:48 +0200

    Put arrow function in process_solution script

--- src/optimization_pulp/main_optimization_pulp.py
+++ src/optimization_pulp/main_optimization_pulp.py
@@ -32,7 +32,7 @@ def run_optimization(start_day="2020-3-28",
     # --------------------------------------------------------------------------- #
     # Read data
     # --------------------------------------------------------------------------- #  
-    import read_data_for_optimization_pulp 
+    from src.optimization_pulp import read_data_for_optimization_pulp 
     current_df, trend_dict = read_data_for_optimization_pulp.read_and_process_data(trend_data = path_to_trend_data,
                                                                               region_data = path_to_static_region_data,
                                                                               today = today)
@@ -42,13 +42,13 @@ def run_optimization(start_day="2020-3-28",
     # Build and prepare optimization model
     # --------------------------------------------------------------------------- # 
     import pulp as plp
-    import build_optimization_model_pulp
+    from src.optimization_pulp import build_optimization_model_pulp
     mdl = build_optimization_model_pulp.build_model()
     mdl = build_optimization_model_pulp.define_model_parameters_and_sets(mdl, current_df)
     mdl = build_optimization_model_pulp.define_model_variables(mdl)
     mdl = build_optimization_model_pulp.calculate_distances(mdl, current_df,path_to_static_distances)
     
-    import define_model_constraints_pulp
+    from src.optimization_pulp import define_model_constraints_pulp
     mdl = define_model_constraints_pulp.exempt_departments(mdl)
     mdl = define_model_constraints_pulp.set_initial_state(mdl,current_df)
     mdl = define_model_constraints_pulp.link_x_and_y_vars(mdl)
@@ -58,7 +58,7 @@ def run_optimization(start_day="2020-3-28",
     mdl = define_model_constraints_pulp.short_transfers_per_dep(mdl)
     mdl = define_model_constraints_pulp.update_for_next_period(mdl,trend_dict,today,target_day)
     
-    import define_model_objective_pulp
+    from src.optimization_pulp import define_model_objective_pulp
     mdl = define_model_objective_pulp.define_max_overcapacity(mdl,current_df)
     mdl = define_model_objective_pulp.define_distance_measures(mdl)
     mdl = define_model_objective_pulp.summarize_objectives(mdl,
@@ -80,7 +80,7 @@ def run_optimization(start_day="2020-3-28",
     # --------------------------------------------------------------------------- #
     # Process solution
     # --------------------------------------------------------------------------- # 
-    import process_solution_pulp
+    from src.optimization_pulp import process_solution_pulp
     mdl = process_solution_pulp.process_allocations(mdl)
     mdl,current_df = process_solution_pulp.process_final_data(mdl,current_df,trend_dict,today,target_day)
     final_map_without_opt = process_solution_pulp.plot_final_state_without_opt(mdl,current_df,geojson=path_to_static_geojson)
